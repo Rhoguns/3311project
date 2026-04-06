a950ffda7f2bfa6eeac4d89f7699e3e232271c36
TuLe12 <tim961@my.yorku.ca>
2026-04-05 20:54:23 -0400

    Delete backend/src/admin directory for restructuring

--- backend/src/admin/PolicyCommand.java
+++
@@ -1,9 +0,0 @@
-package admin;
-public interface PolicyCommand {
-	/**
-	 * Command interface which allows the invocation of unique policy commands
-	 * that allow the policies to be updated. Uses the command software design pattern.
-	 * @return a boolean value indicating a successful command call
-	 */
-	public boolean update();
-}
